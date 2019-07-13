854e3d73a41b96b73a5e97263fca83fd4639a2bd
esaki01 <social0430@gmail.com>
2019-07-13 14:32:17 +0900

    add template reference

--- src/pipeline.py
+++ src/pipeline.py
@@ -20,6 +20,19 @@ class MyOptions(PipelineOptions):
             default='gs://dataflow-reference/sink/output.txt',
             help='Output gcs file path.')
 
+        parser.add_value_provider_argument(
+            '--suffix',
+            default='!',
+            help='Output gcs file path.')
+
+
+class MyReplaceFn(beam.DoFn):
+    def __init__(self, vp_suffix):
+        self._vp_suffix = vp_suffix
+
+    def process(self, element):
+        yield element.replace('.', self._vp_suffix.get())
+
 
 if __name__ == '__main__':
     options = MyOptions()
@@ -29,13 +42,14 @@ if __name__ == '__main__':
     google_cloud_options.job_name = 'myjob'  # 任意のジョブ名
     google_cloud_options.staging_location = 'gs://dataflow-reference/binaries'  # 作成したバケット以下の任意のロケーション
     google_cloud_options.temp_location = 'gs://dataflow-reference/tmp'  # 作成したバケット以下の任意のロケーション
+    google_cloud_options.template_location = 'gs://dataflow-reference/template'  # テンプレートのファイルを保存する場所を指定する
     options.view_as(StandardOptions).runner = 'DataflowRunner'  # ランナーには「DataflowRunner」を指定
 
     p = beam.Pipeline(options=options)
 
     (p
      | 'ReadFromText' >> beam.io.ReadFromText(options.input)  # オプションで指定したパスのファイルを読み取る
-     | 'ComputeWordLength' >> beam.Map(lambda element: len(element))  # 1行ごとに文字数を数える
+     | 'ReplaceSuffix' >> beam.ParDo(MyReplaceFn(options.suffix))  # 接尾辞を「.」から指定したものに入れ替える
      | 'WriteToText' >> beam.io.WriteToText(options.output))  # オプションで指定したパスにファイルを書き込む
 
     p.run()
